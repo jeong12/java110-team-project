d89956daeaf17f07c32264586f56747d2b31ebd4
lyc <hellocworld@naver.com>
2018-10-05 15:28:59 +0900

    App 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        
+        System.out.println("오");
+        System.out.println("호");
+        System.out.println("라");
+        System.out.println("...");
     }
 }
---
+++ src/main/java/Test99.java
@@ -0,0 +1,6 @@
+
+public class Test99 {
+    public static void main(String[] args) {
+        System.out.println("이용찬");
+    }
+}
