d6d207df95301565a077bdb0d155c41001d65368
lyc <hellocworld@naver.com>
2018-10-05 15:57:12 +0900

    안녕?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,6 @@
 public class App {
-    String danger="담배 좀 그만펴연 일찍죽으니깐";
-    public String getGreeting() {
-        return danger;
-        //올린건가??
-
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        
-        System.out.println("오");
-        System.out.println("호");
-        System.out.println("라");
-        System.out.println("...");
-        System.out.println("집집집집");
-        
-        System.out.println("갈땐 가더라도 담배한대정도는 괜잖아?");
+        System.out.println("나도 하고싶다아~");
     }
+    
 }
