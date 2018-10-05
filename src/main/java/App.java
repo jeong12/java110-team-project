99437f5391d08ab39e76ac14c9a68f0bf0c6c768
taehyeong <leeth4115@naver.com>
2018-10-05 15:52:11 +0900

    test1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
-	String name="니노막희무스"
-	String danger="담배 좀 그만펴연"
+	String name="니노막희무스";
+	String danger="담배 좀 그만펴연";
     String result="담배 좀 그만펴연 일찍죽으니깐 ..... ㄴ싫어용!";
     public String getGreeting() {
         return danger;
