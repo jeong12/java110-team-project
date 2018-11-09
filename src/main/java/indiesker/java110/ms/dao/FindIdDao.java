8650f6d02c98228273e1c8259e99eebfc7313d73
asutanukey <arc4tv@gmail.com>
2018-11-09 18:18:42 +0900

    아이디찾기

---
+++ src/main/java/indiesker/java110/ms/dao/FindIdDao.java
@@ -0,0 +1,5 @@
+package indiesker.java110.ms.dao;
+
+public interface FindIdDao {
+  String id(String email);
+}
---
+++ src/main/java/indiesker/java110/ms/service/FindIdService.java
@@ -0,0 +1,5 @@
+package indiesker.java110.ms.service;
+
+public interface FindIdService {
+   String getId(String email);
+}
