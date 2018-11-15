8135620a5f4ae97e9117dcfe185a0351b58ea102
g <rhkdgh0418@daum.net>
2018-11-15 19:08:36 +0900

    관리자

--- src/main/java/indiesker/java110/ms/web/MemberManagerController.java
+++ src/main/java/indiesker/java110/ms/web/MemberManagerController.java
@@ -1,5 +1,8 @@
 package indiesker.java110.ms.web;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.servlet.ServletContext;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import indiesker.java110.ms.domain.MemberManager;
 import indiesker.java110.ms.service.MemberManagerService;
 
@@ -25,7 +29,7 @@ public class MemberManagerController {
   @GetMapping("list")
   public void list(
           @RequestParam(defaultValue="1") int pageNo,
-          @RequestParam(defaultValue="3") int pageSize,
+          @RequestParam(defaultValue="10") int pageSize,
           Model model) {
 
       if (pageNo < 1)
@@ -41,7 +45,7 @@ public class MemberManagerController {
   @GetMapping("select")
   public void listSelect(char flag,
           @RequestParam(defaultValue="1") int pageNo,
-          @RequestParam(defaultValue="3") int pageSize,
+          @RequestParam(defaultValue="10") int pageSize,
           Model model) {
 
       if (pageNo < 1)
@@ -49,9 +53,51 @@ public class MemberManagerController {
 
       if (pageSize < 3 || pageSize > 10)
           pageSize = 3;
-      
       List<MemberManager> select = memberManagerService.listSelect(flag ,pageNo, pageSize);
+      
       model.addAttribute("select", select);
+  }
+  
+  @ResponseBody
+  @RequestMapping(value="dateselect")
+  public List<MemberManager> dateSelect(String flag, String text,String cdt1, String cdt2,
+          @RequestParam(defaultValue="1") int pageNo,
+          @RequestParam(defaultValue="10") int pageSize,
+          Model model) throws ParseException {
+        
+/*    
+    DateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    String ncdt1= sdFormat.format(cdt1);
+    String ncdt2= sdFormat.format(cdt2);*/
+    
+/*    Date ncdt1 = Date.valueOf(cdt1);
+    Date ncdt2 = Date.valueOf(cdt2);
+         */
+    
+    
+    System.out.println(flag);
+    System.out.println(cdt1);
+    System.out.println(cdt2);
+    System.out.println(text);
+      if (pageNo < 1)
+          pageNo = 1;
+      
+      if (pageSize < 3 || pageSize > 10)
+          pageSize = 3;
+      
+      
+      
+      List<MemberManager> list =memberManagerService.dateSelect(flag,text,cdt1,cdt2,pageNo, pageSize);
+     
+        
+      DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+      for(MemberManager m:list) {
+        m.setNcdt(format.format(m.getCdt()));
+      }
+      
+      return list; 
   }
   
 
